58730e1cb35df5a2d874ee88e3bf524880175dcc
sergiosssg <sergiosssg@gmail.com>
2021-12-28 16:41:34 +0200

    Checked out  working ORM mechanizm of Entity Framework in console output in method   MainWindow  of class   MainWindow.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,8 +25,85 @@ namespace WForm001
     {
         public MainWindow()
         {
-            ;
+            Console.WriteLine("Привет Мир!");
+            string strConnectionString = makeCoonectionString2SQL();
+            connToDBEntity(strConnectionString);
             InitializeComponent();
         }
+
+        private static string makeCoonectionString2SQL()
+        {
+
+            //string strExePath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+            System.Data.Common.DbConnectionStringBuilder dbConnectionStringBuilder = new SqlConnectionStringBuilder();
+
+
+            //dbConnectionStringBuilder["ClientLibrary"] = @"C:\Program Files\Firebird\Firebird_2_5\bin\fbclient.dll";
+
+            dbConnectionStringBuilder["Data Source"] = "localhost";
+            //dbConnectionStringBuilder["Initial Catalog"] = @"C:\SSG\PROJECTs\TELET\DB4TELEFONE\sampd_cexs.fdb";//"sampd_cexs";
+            //dbConnectionStringBuilder["Database"] = Path.Combine(strExePath, "tmp.fdb");
+            dbConnectionStringBuilder["Database"] = "sampd_cexs";
+
+            //dbConnectionStringBuilder["User ID"] = "sysdba";
+            //dbConnectionStringBuilder["Password"] = "masterkey";
+
+            //dbConnectionStringBuilder["Charset"] = "UTF8";
+
+
+            //dbConnectionStringBuilder["Embedded"] = FbServerType.Embedded;
+            dbConnectionStringBuilder["integrated Security"] = "SSPI";
+
+            return dbConnectionStringBuilder.ConnectionString;
+
+        }
+
+
+        private static DbContext connToDBEntity(string sConnectionString)
+        {
+            try
+            {
+                using (var dbContent = new DbAppContext(sConnectionString))
+                {
+
+
+
+                    /*var simpleQueryOfVidConnects = dbContent.pO_TEL_VID_CONNECTs.Where(s => s.Id > 0);
+
+
+                    Console.WriteLine("=================================================");
+                    foreach (var oneElement in simpleQueryOfVidConnects)
+                    {
+                        Console.WriteLine(" Id = {0}  Kod связи {1}  Название вида связи {2}", oneElement.Id, oneElement.KodOfConnect, oneElement.Name);
+                    }
+                    Console.WriteLine("=================================================");*/
+
+
+
+
+                    var simpleVidConnects = dbContent.pO_TEL_VID_CONNECTs;
+
+
+                    foreach (var oneTEL_VID_CONNECT in simpleVidConnects)
+                    {
+                        Console.WriteLine(" Id = {0}  Kod связи {1}  Название вида связи {2}", oneTEL_VID_CONNECT.Id, oneTEL_VID_CONNECT.KodOfConnect, oneTEL_VID_CONNECT.Name);
+
+                    }
+
+
+
+                    return dbContent;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+
+
     }
 }
